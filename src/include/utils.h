6ef67e88bceb1adffd5b3f373d67678ed4ddfd65
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-04-21 21:40:37 -0700

    add vec3

--- src/include/utils.h
+++ src/include/utils.h
@@ -7,12 +7,18 @@ typedef struct vec2 {
     double y;
 } vec2;
 
+typedef struct vec3 {
+    double x;
+    double y;
+    double z;
+} vec3;
+
 double vec2_mag(vec2 vec);
 double vec2_mag_sq(vec2 vec);
-vec2 vec2_unit(vec2 vec);
-void vec2_unit_ip(vec2 *vec);
 double vec2_angle(vec2 vec);
 double vec2_angle_deg(vec2 vec);
+vec2 vec2_unit(vec2 vec);
+void vec2_unit_ip(vec2 *vec);
 vec2 vec2_proj(vec2 term1, vec2 term2);
 void vec2_proj_ip(vec2 *term1, vec2 term2);
 vec2 vec2_add(vec2 addend1, vec2 addend2);
@@ -38,6 +44,51 @@ double vec2_cross(vec2 term1, vec2 term2);
 double vec2_dist(vec2 term1, vec2 term2);
 double vec2_dist_sq(vec2 term1, vec2 term2);
 double vec2_angle_to(vec2 term1, vec2 term2);
+
+double vec3_mag(vec3 vec);
+double vec3_mag_sq(vec3 vec);
+double vec3_angle_x(vec3 vec);
+double vec3_angle_x_deg(vec3 vec);
+double vec3_angle_y(vec3 vec);
+double vec3_angle_y_deg(vec3 vec);
+double vec3_angle_z(vec3 vec);
+double vec3_angle_z_deg(vec3 vec);
+vec3 vec3_unit(vec3 vec);
+void vec3_unit_ip(vec3 *vec);
+vec3 vec3_proj(vec3 term1, vec3 term2);
+void vec3_proj_ip(vec3 *term1, vec3 term2);
+vec3 vec3_add(vec3 addend1, vec3 addend2);
+void vec3_add_ip(vec3 *addend1, vec3 addend2);
+vec3 vec3_sub(vec3 minuend, vec3 subtrahend);
+void vec3_sub_ip(vec3 *minuend, vec3 subtrahend);
+vec3 vec3_mul(vec3 multiplicand, double multiplier);
+void vec3_mul_ip(vec3 *multiplicand, double multiplier);
+vec3 vec3_div(vec3 dividend, double divisor);
+void vec3_div_ip(vec3 *dividend, double divisor);
+vec3 vec3_scale(vec3 vec, double mag);
+void vec3_scale_ip(vec3 *vec, double mag);
+vec3 vec3_lerp(vec3 term1, vec3 term2, double t);
+void vec3_lerp_ip(vec3 *term1, vec3 term2, double t);
+vec3 vec3_mov(vec3 term1, vec3 term2, double disp);
+void vec3_mov_ip(vec3 *term1, vec3 term2, double disp);
+vec3 vec3_rot_x(vec3 vec, double angle);
+void vec3_rot_x_ip(vec3 *vec, double angle);
+vec3 vec3_rot_x_deg(vec3 vec, double angle);
+void vec3_rot_x_deg_ip(vec3 *vec, double angle);
+vec3 vec3_rot_y(vec3 vec, double angle);
+void vec3_rot_y_ip(vec3 *vec, double angle);
+vec3 vec3_rot_y_deg(vec3 vec, double angle);
+void vec3_rot_y_deg_ip(vec3 *vec, double angle);
+vec3 vec3_rot_z(vec3 vec, double angle);
+void vec3_rot_z_ip(vec3 *vec, double angle);
+vec3 vec3_rot_z_deg(vec3 vec, double angle);
+void vec3_rot_z_deg_ip(vec3 *vec, double angle);
+vec3 vec3_cross(vec3 term1, vec3 term2);
+void vec3_cross_dest(vec3 term1, vec3 term2, vec3 *dest);
+double vec3_dot(vec3 term1, vec3 term2);
+double vec3_dist(vec3 term1, vec3 term2);
+double vec3_dist_sq(vec3 term1, vec3 term2);
+double vec3_angle_to(vec3 term1, vec3 term2);
 
 double hypot(double x, double y);
 
